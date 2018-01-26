c35fd934fd80868b64fa925732f2a87f9080d247
Brady Davis <bradydavis.ver.2.0@gmail.com>
2018-01-26 15:37:28 -0600

    Adding Nickel test to Accept coin test
    Test that Nickel Insert updates display. Added setup to test.

--- src/kata/tests/AcceptCoinsTestSuite.java
+++ src/kata/tests/AcceptCoinsTestSuite.java
@@ -3,16 +3,30 @@ package kata.tests;
 import static org.junit.Assert.*;
 import kata.vendingMachine.VendingMachine;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class AcceptCoinsTestSuite 
 {
-
+	private VendingMachine vendingMachine;
+	
+	@Before
+	public void setUp()
+	{
+		vendingMachine = new VendingMachine();
+	}
+	
 	@Test
 	public void whenVendingMachineHasNoCoinsInsertedDisplayShowsINSERTCOIN() 
 	{
-		VendingMachine vendingMachine = new VendingMachine();
 		assertEquals("INSERT COIN", vendingMachine.getDisplayMessage());
+	}
+	
+	@Test
+	public void whenVendingMachineAcceptsANickelItUpdatesDisplayToShow5Cents()
+	{
+		vendingMachine.insertCoin(new Nickel());
+		assertEquals(".05", vendingMachine.getDisplayMessage());
 	}
 
 }
